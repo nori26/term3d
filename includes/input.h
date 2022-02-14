fbb9b513d6662e7d229f2a148c6721ed3b412696
nori <nori623p@gmail.com>
2022-02-14 17:10:10 +0100

    add validate_num_of_fields

--- includes/input.h
+++ includes/input.h
@@ -7,5 +7,7 @@ typedef struct s_clist t_clist;
 
 t_clist	*input_lines(FILE *infile);
 t_vect	*input(char *filename);
+void	validate_lines(t_clist *lines);
+void	validate_num_of_fields(char *line, size_t line_no);
 
 #endif
--- srcs/input/input.c
+++ srcs/input/input.c
@@ -3,27 +3,44 @@
 #include <stdbool.h>
 #include <errno.h>
 #include "ft_list.h"
+#include "ft_string.h"
+#include "ft_io.h"
 #include "vector.h"
 #include "wrapper.h"
 
-// bool	validate_infile(FILE *infile)
-// {
-// 	char	*line;
-// 	size_t	n;
+#define NUM_OF_DIMENSIONS 3
 
-// 	n = 0;
-// 	line = NULL;
-// 	while (true)
-// 	{
-// 		errno = 0;
-// 		if (ft_getline(&line, &n, infile) == -1)
-// 			break ;
-// 		is_valid_num_of_fields(infile);
-// 		free(line);
-// 		line = NULL;
-// 	}
-// 	return (errno == 0);
-// }
+void	validate_num_of_fields(char *line, size_t line_no)
+{
+	size_t	fields;
+
+	fields = 1;
+	while (*line)
+		if (*line++ == ',')
+			fields++;
+	if (fields != NUM_OF_DIMENSIONS)
+	{
+		fprintf(stderr, "line: %zu invalid fields\n", line_no);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	validate_lines(t_clist *lines)
+{
+	char	*line;
+	size_t	line_no;
+
+	line_no = 0;
+	lines = ft_clstfirst(lines);
+	while (!ft_clst_isend(lines))
+	{
+		line_no++;
+		line = lines->data;
+		validate_num_of_fields(line, line_no);
+		// validate_fields(line, line_no);
+		lines = lines->next;
+	}
+}
 
 t_clist	*input_lines(FILE *infile)
 {
@@ -36,7 +53,7 @@ t_clist	*input_lines(FILE *infile)
 		exit(EXIT_FAILURE);
 	n = 0;
 	line = NULL;
-	while (getline(&line, &n, infile) != -1)
+	while (ft_getline(&line, &n, infile) != -1)
 	{
 		if (!ft_clstnew_add_back(lines, line))
 			exit(EXIT_FAILURE);
@@ -47,13 +64,17 @@ t_clist	*input_lines(FILE *infile)
 
 t_vect	*input(char *filename)
 {
+	t_vect	*points;
 	FILE	*infile;
 	t_clist	*lines;
 
 	(void)lines;
+	(void)points;
+	// validate_filename();
 	infile = ft_fopen(filename, "r");
 	lines = input_lines(infile);
-	// validate_lines(lines);
+	validate_lines(lines);
+	// points = parse_lines_to_vector();
 	ft_clst_clear(&lines, free);
 	return (NULL);
 }
