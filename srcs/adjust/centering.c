540953280bf00c1d004528521eba045df7a13d6e
nori <nori623p@gmail.com>
2022-02-17 03:24:02 +0100

    refactor offset

--- srcs/adjust/centering.c
+++ srcs/adjust/centering.c
@@ -2,21 +2,30 @@
 #include "term3d.h"
 #include "vector.h"
 
-static t_vect	get_offest_from_origin(t_points *points)
+static t_vect	sum_all_points(t_points *points)
 {
-	t_vect	offset;
+	t_vect	sum;
 	t_vect	*vect;
 	size_t	i;
 
-	offset = (t_vect){};
+	sum = (t_vect){};
 	i = 0;
 	while (i < points->size)
 	{
 		vect = &points->vects[i];
-		offset = vect_add(offset, *vect);
+		sum = vect_add(sum, *vect);
 		i++;
 	}
-	offset = vect_mult(offset, 1.0 / points->size);
+	return (sum);
+}
+
+static t_vect	get_offest_from_origin(t_points *points)
+{
+	t_vect	sum;
+	t_vect	offset;
+
+	sum = sum_all_points(points);
+	offset = vect_mult(sum, 1.0 / points->size);
 	return (offset);
 }
 
