21014e978f9930020788a4977e75d0eec815073c
nori <nori623p@gmail.com>
2022-02-19 02:52:13 +0100

    fix set_rotation

--- srcs/draw/rotate_util.c
+++ srcs/draw/rotate_util.c
@@ -6,26 +6,47 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 11:11:19 by user42            #+#    #+#             */
-/*   Updated: 2022/02/18 13:54:58 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2022/02/19 02:49:39 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
-#include <stdio.h>
 #include "phi.h"
+#include "draw.h"
 
-double	get_angle_w_s(double angle, char option)
+int	get_angle_sign(double angle)
 {
-	double			abs;
+	if (angle > 0)
+		return (1);
+	else
+		return (-1);
+}
+
+double	update_angle_level(double angle, char option)
+{
+	double	abs;
+	int		sign;
 
 	abs = fabs(angle);
+	sign = get_angle_sign(angle);
 	if (option == 's' && abs > 0)
 		abs -= PHI;
 	else if (option == 'w' && abs < M_PI * 2)
 		abs += PHI;
-	if (angle > 0)
-		angle = abs;
+	return (abs * sign);
+}
+
+double	toggle_rotation(double angle)
+{
+	static double	prev;
+	double			new_angle;
+
+	if (angle)
+	{
+		prev = get_rotation_angle();
+		new_angle = 0;
+	}
 	else
-		angle = abs * -1;
-	return (angle);
+		new_angle = prev;
+	return (new_angle);
 }
