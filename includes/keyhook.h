c1b946273bf728de0191ede68417e1a8bc5de160
kazu-42 <91968271+kazu-42@users.noreply.github.com>
2022-02-19 10:10:28 +0900

    cahr → int (int getchar(void))

--- includes/keyhook.h
+++ includes/keyhook.h
@@ -16,11 +16,11 @@
 typedef struct s_option
 {
 	bool	reset;
-	char	rotate_speed;
-	char	zoom_rate;
+	int		rotate_speed;
+	int		zoom_rate;
 }	t_option;
 
 void	hook_key_input(t_option *option);
-void	parse_option(char key_input, t_option *option);
+void	parse_option(int key_input, t_option *option);
 
 #endif
