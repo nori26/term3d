dbecbaf119731192b4a2a148909ae5175c642475
nori <nori623p@gmail.com>
2022-02-19 16:21:52 +0100

    fix charset

--- srcs/draw/print.c
+++ srcs/draw/print.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42    </var/mail/user42>               +#+  +:+       +#+        */
+/*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 09:27:09 by user42            #+#    #+#             */
-/*   Updated: 2022/02/17 09:27:09 by user42           ###   ########.fr       */
+/*   Updated: 2022/02/19 16:20:35 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 #include <string.h>
 #include "draw.h"
 
-#define TERM_CLEAR "\033c"
+#define TERM_CLEAR 			"\033c"
+#define SCREEN_CHAR_SET		" :;=co+xanm."
 
 static char	get_screen_char(size_t density)
 {
-	const char		*set = " :;=co+xanm.";
+	const char		*set = SCREEN_CHAR_SET;
 	const size_t	size = strlen(set);
 	size_t			idx;
 
